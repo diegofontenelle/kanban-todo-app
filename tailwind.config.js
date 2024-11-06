09617fd47456e4df14c2a267dc0302ed224eecd6
Diego Fontenelle <diegolimafontenelle@gmail.com>
2024-11-06 11:45:27 -0300

    style: add custom box-shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "all-around": "0 0 12px 0.5px rgba(0, 0, 0, 0.3)",
+      },
+    },
   },
   plugins: [],
 };
